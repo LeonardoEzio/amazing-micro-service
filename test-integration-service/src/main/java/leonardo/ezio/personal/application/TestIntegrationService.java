25819abbcd5fedb8817436a276792ca542169a6e
LeonardoEzio <LeonardoEzio@163.com>
2024-04-01 22:15:25 +0800

    初始化仓库

---
+++ test-integration-service/src/main/java/leonardo/ezio/personal/application/TestIntegrationService.java
@@ -0,0 +1,16 @@
+package leonardo.ezio.personal.application;
+
+import org.springframework.boot.SpringApplication;
+import org.springframework.boot.autoconfigure.SpringBootApplication;
+
+/**
+ * Hello world!
+ */
+@SpringBootApplication
+public class TestIntegrationService {
+
+    public static void main(String[] args) {
+        SpringApplication.run(TestIntegrationService.class);
+    }
+
+}
